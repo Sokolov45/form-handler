edd5252cf46a438dc1c215fc38a1762795810b90
Sokolov45 <zvuk45@bk.ru>
2020-12-14 22:41:48 +0500

    add structure

--- order.php
+++ order.php
@@ -11,12 +11,30 @@ $name = $_POST['name'];
 $tel = $_POST['tel'];
 $address = '';
 $fields = ['street', 'home', 'korpus', 'flat', 'floor'];
-
-$burger = new Burger();
-$user = $burger->getUserbyEmail();
-if ($user) {
-
-} else {
-
+foreach ($_POST as $key => $value) {
+    if ($value && in_array($value, $fields)) {
+        $address .= ',' . "$value";
+    }
 }
 
+$burger = new Burger();
+$user = $burger->getUserByEmail();
+if ($user) {
+    $userId = $user[id];
+//    должен вернуть id заказа
+    $orderId = $burger->addOrder($userId);
+
+    $burger->incOrders();
+    $orders_count = $user[orders_count] + 1;
+
+    //дату заказа запишем через sql
+
+} else {
+    //создаём юзера и должны вернуть его id
+    $userId = $burger->createUser();
+    $burger->addOrder($userId);
+}
+
+echo "Спасибо, ваш заказ будет доставлен по адресу: “$address”
+Номер вашего заказа: $orderId
+Это ваш $orders_count -й заказ!";
--- php/burger.php
+++ php/burger.php
@@ -2,5 +2,22 @@
 
 class Burger
 {
+    public function getUserByEmail()
+    {
+
+    }
+
+    public function incOrders()
+    {
+
+    }
+    public function createUser()
+    {
+
+    }
+    public function addOrder()
+    {
+
+    }
 
 }
